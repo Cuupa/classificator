d8df13cb2432777b330be6569a894c662a63e20c
Cuupa <simonthiel2@gmail.com>
2021-02-12 01:37:11 +0100

    mapping month names. WIP

--- src/main/java/com/cuupa/classificator/services/kb/semantic/dataExtraction/DateExtract.kt
+++ src/main/java/com/cuupa/classificator/services/kb/semantic/dataExtraction/DateExtract.kt
@@ -3,23 +3,50 @@ package com.cuupa.classificator.services.kb.semantic.dataExtraction
 import com.cuupa.classificator.constants.RegexConstants
 import java.util.regex.Pattern
 
-class DateExtract(regex: String) : Extract(Pattern.compile(regex)) {
+class DateExtract(regex: String) : Extract(Pattern.compile(regex, Pattern.CASE_INSENSITIVE)) {
 
+    // TODO: normalize months in non numeric values
     override fun normalize(value: String): String {
-        val dateFields = value.split(RegexConstants.dotPattern)
+        if (value.contains(".")) {
+            val dateFields = value.split(RegexConstants.dotPattern)
+            return when (dateFields.size) {
+                3 -> normalizeDate(dateFields)
+                2 -> parseAndNormalizeDate(dateFields)
+                else -> value
+            }
+        }
+        return value
+    }
+
+    private fun parseAndNormalizeDate(dateFields: List<String>): String {
+        var day = dateFields[0]
+        var monthAndYear = dateFields[1].split(" ")
+        var month = Months.get(monthAndYear[0])
+        var year = monthAndYear[1]
+        return fill(day, month, year)
+    }
+
+    private fun fill(day: String, month: String, year: String): String {
+        var day1 = day
+        var month1 = month
+        var year1 = year
+        if (day1.length == 1) {
+            day1 = "0$day1"
+        }
+        if (month1.length == 1) {
+            month1 = "0$month1"
+        }
+        if (year1.length == 2) {
+            year1 = "20$year1"
+        }
+        return "$day1.$month1.$year1"
+    }
+
+    private fun normalizeDate(dateFields: List<String>): String {
         var day = dateFields[0]
         var month = dateFields[1]
         var year = dateFields[2]
-        if (day.length == 1) {
-            day = "0$day"
-        }
-        if (month.length == 1) {
-            month = "0$month"
-        }
-        if (year.length == 2) {
-            year = "20$year"
-        }
-        return "$day.$month.$year"
+        return fill(day, month, year)
     }
 
     companion object {
---
+++ src/main/java/com/cuupa/classificator/services/kb/semantic/dataExtraction/Months.kt
@@ -0,0 +1,12 @@
+package com.cuupa.classificator.services.kb.semantic.dataExtraction
+
+object Months {
+    fun get(value: String): String {
+        val month = value.trim()
+        when {
+
+        }
+
+        return month
+    }
+}
