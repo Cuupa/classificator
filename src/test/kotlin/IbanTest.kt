b821e54c9c0c0ef9dae5b2ff6da825a54adfbc9d
Cuupa <simonthiel2@gmail.com>
2021-05-18 17:03:21 +0200

    moved from src/main/java to src/main/kotlin

---
+++ src/test/kotlin/IbanTest.kt
@@ -0,0 +1,21 @@
+import com.cuupa.classificator.knowledgebase.services.dataExtraction.IbanExtract
+import org.junit.jupiter.api.Test
+import java.util.stream.IntStream
+
+class IbanTest {
+
+    val unitToTest =
+        IbanExtract("[a-z]{2}[0-9]{2}[\\s]?[0-9]{4}[\\s]?[0-9]{4}[\\s]?[0-9]{4}[\\s]?[0-9]{4}[\\s]?[0-9]{2}")
+
+    @Test
+    fun test() {
+        val pattern = unitToTest.regex
+        val result = pattern.find(IBANTestData.normalizedIBAN)
+        val groupValues = result?.groupValues
+        groupValues?.forEach { println(unitToTest.normalize(it)) }
+
+        val result2 = pattern.find(IBANTestData.withoutWhiteSpaces)
+        val groupValues2 = result2?.groupValues
+        groupValues2?.forEach { println(unitToTest.normalize(it)) }
+    }
+}
---
+++ src/test/kotlin/com/cuupa/classificator/knowledgebase/TextExtractorTest.kt
@@ -0,0 +1,5 @@
+package com.cuupa.classificator.knowledgebase
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class TextExtractorTest
