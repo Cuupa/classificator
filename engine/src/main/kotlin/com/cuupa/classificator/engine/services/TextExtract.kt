db758a739a27321a56c788ac65ca33ae17da39bf
simon.thiel2+github@gmail.com <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>
2022-01-10 16:53:47 +0100

    saving the original DOCUMENT content, not the content of the file which could be a zip file

--- engine/src/main/kotlin/com/cuupa/classificator/engine/services/TextExtract.kt
+++ engine/src/main/kotlin/com/cuupa/classificator/engine/services/TextExtract.kt
@@ -2,6 +2,8 @@ package com.cuupa.classificator.engine.services
 
 class TextExtract {
 
+    var contentBytes: ByteArray = ByteArray(0)
+
     var content: String? = null
 
     var contentType: String = "application/octet-stream"
--- engine/src/main/kotlin/com/cuupa/classificator/engine/services/TextExtractor.kt
+++ engine/src/main/kotlin/com/cuupa/classificator/engine/services/TextExtractor.kt
@@ -7,33 +7,73 @@ import org.apache.tika.Tika
 import org.apache.tika.metadata.Metadata
 import org.springframework.http.MediaType
 import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.util.zip.ZipInputStream
 
 class TextExtractor(val tika: Tika) {
+
+    fun extract(contentType: String?, content: ByteArray?): List<TextExtract> {
+        return if ("application/zip" == contentType) {
+            val zipEntries = getZipEntries(content)
+            zipEntries.map { extractText(null, it) }
+        } else {
+            listOf(extractText(contentType, content))
+        }
+    }
+
+    private fun getZipEntries(content: ByteArray?): MutableList<ByteArray> {
+        val buffer = ByteArray(1024)
+        val zipEntries = mutableListOf<ByteArray>()
+
+        ZipInputStream(ByteArrayInputStream(content)).use { zis ->
+            var zipEntry = zis.nextEntry
+            while (zipEntry != null) {
+                if (!zipEntry.isDirectory) {
+                    val out = ByteArrayOutputStream()
+                    var len: Int
+                    while (zis.read(buffer).also { len = it } > 0) {
+                        out.write(buffer, 0, len)
+                    }
+                    zipEntries.add(out.toByteArray())
+                }
+                zipEntry = zis.nextEntry
+            }
+            zis.closeEntry()
+        }
+        return zipEntries
+    }
 
     fun extractText(contentType: String?, content: ByteArray?): TextExtract {
 
         if (content.isNullOrEmpty()) {
             return TextExtract().apply {
+                this.contentBytes = ByteArray(0)
                 this.content = null
                 this.contentType = MediaType.APPLICATION_OCTET_STREAM_VALUE
             }
         }
 
+        val byteContent = content!!
+
         when (contentType) {
             MediaType.APPLICATION_PDF_VALUE -> {
                 PDDocument.load(ByteArrayInputStream(content)).use { document ->
                     return TextExtract().apply {
+                        this.contentBytes = byteContent
                         this.content = document.getText().joinToString(separator = "")
                         this.contentType = contentType
                     }
                 }
             }
             MediaType.TEXT_PLAIN_VALUE -> return TextExtract().apply {
-                this.content = String(content!!)
+                this.contentBytes = ByteArray(0)
+                this.content = String(byteContent)
                 this.contentType = contentType
             }
+
             else -> {
                 return TextExtract().apply {
+                    this.contentBytes = byteContent
                     this.content = tika.parseToString(ByteArrayInputStream(content))
                     this.contentType = tika.detector.detect(ByteArrayInputStream(content), Metadata()).toString()
                 }
