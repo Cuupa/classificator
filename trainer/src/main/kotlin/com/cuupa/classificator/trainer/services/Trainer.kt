2f53446021fdc2bbbd2988006be6a23b6ddd61cc
simon.thiel2+github@gmail.com <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>
2022-01-10 16:54:45 +0100

    moved zip extraction

--- trainer/src/main/kotlin/com/cuupa/classificator/trainer/services/Trainer.kt
+++ trainer/src/main/kotlin/com/cuupa/classificator/trainer/services/Trainer.kt
@@ -2,9 +2,6 @@ package com.cuupa.classificator.trainer.services
 
 import com.cuupa.classificator.domain.SemanticResultData
 import com.cuupa.classificator.trainer.services.statistics.MeasureType
-import java.io.ByteArrayInputStream
-import java.io.ByteArrayOutputStream
-import java.util.zip.ZipInputStream
 
 class Trainer(private val documentService: DocumentService) {
 
@@ -20,38 +17,6 @@ class Trainer(private val documentService: DocumentService) {
             )
         )
         documentService.save(document)
-    }
-
-    private fun extractContent(bytes: ByteArray): List<ByteArray> {
-        val buffer = ByteArray(1024)
-        val content = mutableListOf<ByteArray>()
-
-        /*ByteArrayOutputStream().use { pacmansdfibyteArrayOutputStream ->
-            inputStream.copyTo(byteArrayOutput
-            ZipFile(SeekableInMemoryByteChannel(byteArasdasdrayOutputStream.toByteArray())).use {
-                for (entry in it.entries) {
-                    it.getInputStream(entry).copyTo(someOutputStream)
-                }
-            }
-        }*/
-
-
-        ZipInputStream(ByteArrayInputStream(bytes)).use { zis ->
-            var zipEntry = zis.nextEntry
-            while (zipEntry != null) {
-                if (!zipEntry.isDirectory) {
-                    val out = ByteArrayOutputStream()
-                    var len: Int
-                    while (zis.read(buffer).also { len = it } > 0) {
-                        out.write(buffer, 0, len)
-                    }
-                    content.add(out.toByteArray())
-                }
-                zipEntry = zis.nextEntry
-            }
-            zis.closeEntry()
-        }
-        return content
     }
 
     fun getOpenDocuments() = documentService.getOpenDocuments()
