d4eba2c4517d557e0cacc39e85beaeae466dfe25
simon.thiel2+github@gmail.com <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>
2022-01-15 20:39:39 +0100

    added topic, sender and metadata to csv file

--- ui/src/main/kotlin/com/cuupa/classificator/ui/csv/CsvLine.kt
+++ ui/src/main/kotlin/com/cuupa/classificator/ui/csv/CsvLine.kt
@@ -1,37 +1,48 @@
 package com.cuupa.classificator.ui.csv
 
+import com.cuupa.classificator.engine.extensions.Extension.substringBetween
+import org.springframework.http.MediaType
+
 class CsvLine(headlines: String, line: String) {
 
     val values: Map<String, String>
 
     val contentType: String
-        get() {
-            val value = getValue("contentType")
-            if (value.isEmpty()) {
-                throw IllegalArgumentException("No entry for contentType found")
-            }
-            return value
-        }
+        get() = getValue("contentType") ?: MediaType.TEXT_PLAIN_VALUE
 
     val content: String
         get() {
             val value = getValue("content")
-            if (value.isEmpty()) {
+            if (value.isNullOrEmpty()) {
                 throw IllegalArgumentException("No entry for content found")
             }
             return value
         }
 
-    val tag: String
-        get() {
-            val value = getValue("content")
-            if (value.isEmpty()) {
-                throw IllegalArgumentException("No entry for content found")
-            }
-            return value
-        }
+    val tag: String?
+        get() = getValue("tag")
 
-    private fun getValue(key: String) = values.getOrDefault(key.lowercase(), "").trim()
+    val topics: List<String>
+        get() = getValuesAsList("topics")
+
+    val senders: List<String>
+        get() = getValuesAsList("senders")
+
+    val metadata: List<String>
+        get() = getValuesAsList("metadata")
+            .map { it.replace(" : ", ":") }
+            .map { it.replace(": ", ":") }
+            .map { it.replace(" :", "") }
+
+    private fun getValue(key: String) = values.getOrDefault(key.lowercase(), null)?.trim()
+
+    private fun getValuesAsList(key: String): List<String> {
+        val value = values.getOrDefault(key.lowercase(), null)
+        if (value.isNullOrEmpty()) {
+            return listOf()
+        }
+        return value.substringBetween("[", "]").split(",").map { it.trim() }
+    }
 
     init {
         val headlinesList = headlines.split(";").map { it.lowercase() }.map { it.trim() }.filter { it.isNotEmpty() }
